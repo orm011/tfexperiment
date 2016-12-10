2b4e0f620793f7bc29e8b4e03f60ff9799246d15
Oscar Moll <orm@csail.mit.edu>
2016-12-10 03:24:09 -0500

    weigths for all regs

--- alexnet.py
+++ alexnet.py
@@ -7,6 +7,13 @@ from common import *
 
 FLAGS = tf.app.flags.FLAGS
 
+# l2 weight decay nudges filters to be smoother.
+# (ie, sharp peaks, like those in random noise, are discouraged.)
+# note 2^2 + 2^2  << 1^2 + 3^2
+CONV_WD = 0.0001 # used for conv filters. 0.001 makes them go to 0
+FC_WD = 0.0005 # used for fully conn layers. tried 0.001 earlier.
+BN_WD = 0.0005 # used for batch norm.
+
 #TODO try fp16
 #I would like to monitor gradients etc before going this route (make sure you see if something is wrong)
 #tf.app.flags.DEFINE_boolean('use_fp16', False,
@@ -102,7 +109,7 @@ def batch_normalization(layer, mode, scale_init, local_scope_name):
 
     # with 0.005, all will be between 0.9 and 1.1
     scale = _normal_regularized_cpu_var('batch_norm_scale', shape=[depth],
-                                        mean=1., stddev=0.005, wd=0.0005)
+                                        mean=1., stddev=0.005, wd=BN_WD)
     # never negative
     
     
@@ -169,14 +176,16 @@ def _model(x, keep_dropout, is_training, local_scope_name):
         is_training = Mode.training
     else:
         is_training = Mode.testing
+
     
     # Conv + ReLU + LRN + Pool, 224->55->27
     with tf.variable_scope('conv1') as scope:
-        w = _normal_regularized_cpu_var('weights', shape=[11, 11, 3, 96], wd=0)            
+        w = _normal_regularized_cpu_var('weights', shape=[11, 11, 3, 96], wd=CONV_WD)            
         conv1 = tf.nn.conv2d(x, w, strides=[1, 4, 4, 1], padding='SAME')
 
         # note: bias term gets superseded internal offset of batch norm.
         conv1 = batch_normalization(conv1, is_training, scale_init=1., local_scope_name=(local_scope_name, scope))
+        
         conv1 = tf.nn.relu(conv1)
         lrn1 = tf.nn.local_response_normalization(conv1, depth_radius=5, bias=1.0, alpha=1e-4, beta=0.75)
 
@@ -184,7 +193,7 @@ def _model(x, keep_dropout, is_training, local_scope_name):
 
     # Conv + ReLU + LRN + Pool, 27-> 13
     with tf.variable_scope('conv2') as scope:
-        w =  _normal_regularized_cpu_var('weights', [5, 5, 96, 256], wd=0)
+        w =  _normal_regularized_cpu_var('weights', [5, 5, 96, 256], wd=CONV_WD)
         conv2 = tf.nn.conv2d(pool1, w, strides=[1, 1, 1, 1], padding='SAME')
         conv2 = batch_normalization(conv2, is_training, scale_init=1., local_scope_name=(local_scope_name, scope))
         conv2 = tf.nn.relu(conv2)
@@ -194,21 +203,21 @@ def _model(x, keep_dropout, is_training, local_scope_name):
 
     # Conv + ReLU, 13-> 13
     with tf.variable_scope('conv3') as scope:
-        w =  _normal_regularized_cpu_var('weights', [3, 3, 256, 384], wd=0)
+        w =  _normal_regularized_cpu_var('weights', [3, 3, 256, 384], wd=CONV_WD)
         conv3 = tf.nn.conv2d(pool2, w, strides=[1, 1, 1, 1], padding='SAME')
         conv3 = batch_normalization(conv3, is_training, scale_init=1., local_scope_name=(local_scope_name, scope))
         conv3 = tf.nn.relu(conv3)
 
     # Conv + ReLU, 13-> 13
     with tf.variable_scope('conv4') as scope:
-        w =  _normal_regularized_cpu_var('weights', [3, 3, 384, 256], wd=0)
+        w =  _normal_regularized_cpu_var('weights', [3, 3, 384, 256], wd=CONV_WD)
         conv4 = tf.nn.conv2d(conv3, w, strides=[1, 1, 1, 1], padding='SAME')
         conv4 = batch_normalization(conv4, is_training, scale_init=1., local_scope_name=(local_scope_name, scope))
         conv4 = tf.nn.relu(conv4)
 
     # Conv + ReLU + Pool, 13->6
     with tf.variable_scope('conv5') as scope:
-        w = _normal_regularized_cpu_var('weights', [3, 3, 256, 256], wd=0)
+        w = _normal_regularized_cpu_var('weights', [3, 3, 256, 256], wd=CONV_WD)
         conv5 = tf.nn.conv2d(conv4, w, strides=[1, 1, 1, 1], padding='SAME')
         conv5 = batch_normalization(conv5, is_training, scale_init=1., local_scope_name=(local_scope_name, scope))
         conv5 = tf.nn.relu(conv5)
@@ -216,7 +225,7 @@ def _model(x, keep_dropout, is_training, local_scope_name):
         
     # FC + ReLU + Dropout
     with tf.variable_scope('fc6') as scope:
-        w =  _normal_regularized_cpu_var('weights', [7*7*256, 4096], wd=0.0005)
+        w =  _normal_regularized_cpu_var('weights', [7*7*256, 4096], wd=FC_WD)
         fc6 = tf.reshape(pool5, [-1, w.get_shape().as_list()[0]])
         fc6 = tf.matmul(fc6, w)
         fc6 = batch_normalization(fc6, is_training, scale_init=1., local_scope_name=(local_scope_name, scope))
@@ -225,7 +234,7 @@ def _model(x, keep_dropout, is_training, local_scope_name):
     
     # FC + ReLU + Dropout
     with tf.variable_scope('fc7') as scope:
-        w =  _normal_regularized_cpu_var('weights', [4096, 4096], wd=0.0005)
+        w =  _normal_regularized_cpu_var('weights', [4096, 4096], wd=FC_WD)
         fc7 = tf.matmul(fc6, w)
         fc7 = batch_normalization(fc7, is_training, scale_init=1., local_scope_name=(local_scope_name,scope))
         fc7 = tf.nn.relu(fc7)
@@ -238,7 +247,7 @@ def _model(x, keep_dropout, is_training, local_scope_name):
     # but the next layer is not being trained here, so leaving it
     # unnormalized
     with tf.variable_scope('output') as scope:
-        w =  _normal_regularized_cpu_var('weights', [4096, 100], wd=0.001)
+        w =  _normal_regularized_cpu_var('weights', [4096, 100], wd=FC_WD)
         bo =  _zero_cpu_var('bias', [100])
         # keep the logits name as is (used to look up model op)
         out = tf.add(tf.matmul(fc7, w), bo, name='logits')
