3ae0bba0d59a32ef4492b18495f8907a559aefa0
Oscar Moll <orm@csail.mit.edu>
2016-12-12 11:17:32 -0500

    data loader with attributes + cat

--- DataLoader.py
+++ DataLoader.py
@@ -6,6 +6,7 @@ np.random.seed(123)
 import time
 import queue, threading
 from queue import Queue
+import common
 
 # loading data from .h5
 class DataLoaderH5(object):
@@ -20,10 +21,13 @@ class DataLoaderH5(object):
         # read data info from lists
         f = h5py.File(kwargs['data_h5'], "r")
         self.im_set = f['images']
-        self.lab_set = f['labels']
+        self.cat_set = f['categories']
+        self.attr_set = f['attributes']
 
         self.num = self.im_set.shape[0]
-        assert self.im_set.shape[0]==self.lab_set.shape[0], '#images and #labels do not match!'
+        assert self.im_set.shape[0]==self.cat_set.shape[0]
+        assert self.cat_set.shape[0] == self.attr_set.shape[0]
+        
         assert self.im_set.shape[1]==self.load_size, 'Image size error!'
         assert self.im_set.shape[2]==self.load_size, 'Image size error!'
         print(('# Images found:'), self.num)
@@ -39,15 +43,15 @@ class DataLoaderH5(object):
     #the data-loading task
     def load_task(self):
         while True:
-            labels_batch = np.zeros((self.batch_size, 103))
+            category_batch = np.zeros((self.batch_size, 1))
+            attribute_batch = np.zeros((self.batch_size, common.PARAMS.num_scene_attributes))
             images_batch = np.zeros((self.batch_size, self.fine_size, self.fine_size, 3)) 
 
             for i in range(self.batch_size):
                 actual_idx = self.shuffle_array[self._idx]
                 image = self.im_set[actual_idx]
-                sparse_labels = self.lab_set[actual_idx]
-                labels_batch[i, 0] = sparse_labels[0]
-                labels_batch[i, sparse_labels[1:]] = 1
+                category = self.cat_set[actual_idx]
+                attributes = self.attr_set[actual_idx]
 
                 image = image.astype(np.float32)/255. - self.data_mean
                 if self.randomize:
@@ -61,14 +65,17 @@ class DataLoaderH5(object):
                     offset_w = (self.load_size-self.fine_size)//2 #(orm: use int division python3)
 
                 out_image = image[offset_h:offset_h+self.fine_size, offset_w:offset_w+self.fine_size, :]
+
                 images_batch[i, ...] = out_image
+                category_batch[i] = category
+                attribute_batch[i,...] = attributes
 
                 # reinit in the middle of the loop if necessary
                 self._idx += 1
                 if self._idx == self.num:
                     self._reinit()
             
-            self.data_queue.put((images_batch,labels_batch), True)
+            self.data_queue.put((images_batch,category_batch, attribute_batch), True)
 
     def _reinit(self):
         print('started shuffling array')
@@ -81,71 +88,8 @@ class DataLoaderH5(object):
         self._idx = 0
         
     def next_batch(self):
-        (images_batch,labels_batch) = self.data_queue.get(True)        
-        return images_batch, labels_batch
-    
-    def size(self):
-        return self.num
-
-    def reset(self):
-        self._idx = 0
-
-# Loading data from disk
-class DataLoaderDisk(object):
-    def __init__(self, **kwargs):
-
-        self.load_size = int(kwargs['load_size'])
-        self.fine_size = int(kwargs['fine_size'])
-        self.data_mean = np.array(kwargs['data_mean'])
-        self.randomize = kwargs['randomize']
-        self.data_root = os.path.join(kwargs['data_root'])
-
-        # read data info from lists
-        self.list_im = []
-        self.list_lab = []
-        with open(kwargs['data_list'], 'r') as f:
-            for line in f:
-                path, lab =line.rstrip().split(' ')
-                self.list_im.append(os.path.join(self.data_root, path))
-                self.list_lab.append(int(lab))
-        self.list_im = np.array(self.list_im, np.object)
-        self.list_lab = np.array(self.list_lab, np.int64)
-        self.num = self.list_im.shape[0]
-        print(('# Images found:'), self.num)
-
-        # permutation
-        perm = np.random.permutation(self.num) 
-        self.list_im = self.list_im[perm]
-        self.list_lab = self.list_lab[perm]
-
-        self._idx = 0
-        
-    def next_batch(self, batch_size):
-        images_batch = np.zeros((batch_size, self.fine_size, self.fine_size, 3)) 
-        labels_batch = np.zeros(batch_size)
-        for i in range(batch_size):
-            image = scipy.misc.imread(self.list_im[self._idx])
-            image = scipy.misc.imresize(image, (self.load_size, self.load_size))
-            image = image.astype(np.float32)/255.
-            image = image - self.data_mean
-            if self.randomize:
-                flip = np.random.random_integers(0, 1)
-                if flip>0:
-                    image = image[:,::-1,:]
-                offset_h = np.random.random_integers(0, self.load_size-self.fine_size)
-                offset_w = np.random.random_integers(0, self.load_size-self.fine_size)
-            else:
-                offset_h = (self.load_size-self.fine_size)//2 #(orm: use int division python3)
-                offset_w = (self.load_size-self.fine_size)//2 #(orm: use int division python3)
-
-            images_batch[i, ...] =  image[offset_h:offset_h+self.fine_size, offset_w:offset_w+self.fine_size, :]
-            labels_batch[i, ...] = self.list_lab[self._idx]
-            
-            self._idx += 1
-            if self._idx == self.num:
-                self._idx = 0
-        
-        return images_batch, labels_batch
+        (images_batch,category_batch,attribute_batch) = self.data_queue.get(True)        
+        return images_batch, category_batch, attribute_batch
     
     def size(self):
         return self.num
